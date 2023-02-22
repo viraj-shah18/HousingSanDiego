0d99ca8895b0c5ad02c2b58fffc302d0eb3d3c05
hjl2009 <huangjiale2009@126.com>
2023-02-21 20:05:23 -0800

    finished navigation

--- frontend/src/components/homePage.js
+++ frontend/src/components/homePage.js
@@ -1,1 +1,13 @@
 //This file is for homepage (both login and logout)
+import * as React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <text>This is homepage!!</text>
+            <img src={ require('../home.png') } />
+        </div>
+    );
+}
+
+export default HomePage;
--- frontend/src/components/searchPage.js
+++ frontend/src/components/searchPage.js
@@ -1,1 +1,13 @@
 //This page is for searching
+import * as React from 'react';
+
+function SearchPage() {
+    return (
+        <div>
+            <text>This is SearchPage!!</text>
+            <img src={ require('../searchpage.png') } />
+        </div>
+    );
+}
+
+export default SearchPage;
