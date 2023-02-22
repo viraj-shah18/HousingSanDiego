2f9db480db31aa346676d1d31b9fd972904ea237
vicaranq <vdarango@hotmail.com>
2023-02-21 23:00:58 -0800

    Added search bar, however it's not recognizing styles.css . Need to add background image as well

--- frontend/src/components/homePage.js
+++ frontend/src/components/homePage.js
@@ -1,13 +1,40 @@
 //This file is for homepage (both login and logout)
 import * as React from 'react';
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+
 
 function HomePage() {
     return (
-        <div>
-            <text>This is homepage!!</text>
-            <img src={ require('../home.png') } />
-        </div>
+
+        // {/* <div> 
+        //     <text>This is homepage!!!</text>
+        //     <img src={ require('../home.png') } />            
+        // </div>
+        //  */}
+        <Search />
     );
 }
+
+
+function Search({ details }) {
+    const [searchField, setSearchField] = useState("");
+    const [searchShow, setSearchShow] = useState(false);
+  
+    return (
+      <div className="main">
+      <h1>Home Search</h1>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+    </div>
+    );
+  }
+  
 
 export default HomePage;
