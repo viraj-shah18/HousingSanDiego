5814fb3ad748d86831377803d877c8d85aee89fc
Viraj Shah <viraj0605@gmail.com>
2023-03-04 18:28:24 -0800

    added create collections button and formatting

--- frontend/src/components/collection.js
+++ frontend/src/components/collection.js
@@ -4,6 +4,8 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
+import axios from 'axios';
 
 
 const style = {
@@ -15,12 +17,11 @@ const style = {
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
-    p: 4,
-    maxHeight: 400,
-    overflow: 'auto'
+    p: 2,
   };
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#f00',
+  
+const RemoveItem = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#f00',
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
@@ -28,6 +29,14 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
 
+  const AddItem = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#0f0',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: theme.palette.text.secondary,
+  }));
 
 
 export default function Collection () {
@@ -38,89 +47,127 @@ export default function Collection () {
     
     return (
       <Box sx={style}>
-      <Typography id='modal-modal-title' variant='h5' component='h2' align='center'>
-      My Collections
-      </Typography>
-      <Box sx={{ flexGrow: 1 }}>
+        <Typography id='modal-modal-title' variant='h5' component='h2' align='center'>
+        My Collections
+        </Typography>
+        <Divider variant="middle" sx={{ p:1}}/> 
+        <Box sx={{ flexGrow: 1, maxHeight: 300, overflow: 'auto' }}>
           <Grid container spacing={2}>
-            <Grid item xs={4}>
-              <img src={ require('../imgs/home.jpg') } width='200px' height='110px' />
+            <Grid item xs={3}>
+              <img src={ require('../imgs/home.jpg') } width='190px' height='110px' />
             </Grid>
-            <Grid item xs={6}>
-              <Typography align='left'>Houses near UCSD {collection_ids[0]}</Typography>
+            <Grid item xs={7}>
+              <Typography align='left' margin='1.5rem 0px 0px 1rem' fontWeight={'bold'}>Houses near UCSD {collection_ids[0]}</Typography>
+              <Typography align='left' margin='0rem 0px 0px 1rem' fontSize={'12px'}>7 places</Typography>
             </Grid>
             <Grid item xs={2}>
-              <Item>Remove</Item>
+              <RemoveItem>Remove</RemoveItem>
             </Grid>
           </Grid>
 
           <Grid container spacing={2}>
-            <Grid item xs={4}>
-              <img src={ require('../collectionpage.png') } width='200px' height='110px' />
+            <Grid item xs={3}>
+              <img src={ require('../collectionpage.png') } width='190px' height='110px' />
             </Grid>
-            <Grid item xs={6}>
-              <Typography align='left'>Houses near Ralphs {collection_ids[1]}</Typography>
+            <Grid item xs={7}>
+            <Typography align='left' margin='1.5rem 0px 0px 1rem' fontWeight={'bold'}>Houses near UCSD {collection_ids[0]}</Typography>
+            <Typography align='left' margin='0rem 0px 0px 1rem' fontSize={'12px'}>7 places</Typography>
             </Grid>
             <Grid item xs={2}>
-              <Item>Remove</Item>
+              <RemoveItem>Remove</RemoveItem>
             </Grid>
           </Grid>
 
           <Grid container spacing={2}>
-            <Grid item xs={4}>
-              <img src={ require('../home.png') } width='200px' height='110px' />
+            <Grid item xs={3}>
+                <img src={ require('../home.png') } width='190px' height='110px' />
             </Grid>
-            <Grid item xs={6}>
-              <Typography align='left'>3 Bedrooms</Typography>
+            <Grid item xs={7}>
+            <Typography align='left' margin='1.5rem 0px 0px 1rem' fontWeight={'bold'}>Houses near UCSD {collection_ids[0]}</Typography>
+                <Typography align='left' margin='0rem 0px 0px 1rem' fontSize={'12px'}>7 places</Typography>
             </Grid>
             <Grid item xs={2}>
-              <Item>Remove</Item>
+                <RemoveItem>Remove</RemoveItem>
             </Grid>
           </Grid>
 
           <Grid container spacing={2}>
-            <Grid item xs={4}>
-              <img src={ require('../findroommate.png') } width='200px' height='110px' />
+            <Grid item xs={3}>
+              <img src={ require('../findroommate.png') } width='190px' height='110px' />
             </Grid>
-            <Grid item xs={6}>
-              <Typography align='left'>3 Bedrooms</Typography>
+            <Grid item xs={7}>
+            <Typography align='left' margin='1.5rem 0px 0px 1rem' fontWeight={'bold'}>Houses near UCSD {collection_ids[0]}</Typography>
+              <Typography align='left' margin='0rem 0px 0px 1rem' fontSize={'12px'}>7 places</Typography>
             </Grid>
             <Grid item xs={2}>
-              <Item>Remove</Item>
+              <RemoveItem>Remove</RemoveItem>
             </Grid>
           </Grid>
 
           <Grid container spacing={2}>
-            <Grid item xs={4}>
-              <img src={ require('../home.png') } width='200px' height='110px' />
+            <Grid item xs={3}>
+              <img src={ require('../home.png') } width='190px' height='110px' />
             </Grid>
-            <Grid item xs={6}>
-              <Typography align='left'>3 Bedrooms</Typography>
+            <Grid item xs={7}>
+            <Typography align='left' margin='1.5rem 0px 0px 1rem' fontWeight={'bold'}>Houses near UCSD {collection_ids[0]}</Typography>
+              <Typography align='left' margin='0rem 0px 0px 1rem' fontSize={'12px'}>7 places</Typography>
             </Grid>
             <Grid item xs={2}>
-              <Item>Remove</Item>
+              <RemoveItem>Remove</RemoveItem>
             </Grid>
           </Grid>
+      </Box>
 
+      {/* Create new collection */}
+      <Divider variant="middle" sx={{ p:1}}/> 
+      <Box paddingTop={'2rem'} >
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <Typography>
+              Create new collection: 
+            </Typography>
+          </Grid>
+          <Grid item xs={7}>
+            <Typography>
+              Placeholder
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography>
+              <AddItem>Add</AddItem>
+            </Typography>
+          </Grid>
+        </Grid>
       </Box>
   </Box>
-
-
     );
 }   
 
 
+export async function FetchCollections(userId) {
+  let data;
+  await axios.get('http://localhost:8000/api/user_collection/${userId}')
+    .then(res => {
+      console.log({res})
+      data = res.data;
+      console.log({data})
+  })
+  .catch(err => {})
+  return (data);
+}
+
 function collectionList({name, num_places, img}) {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={4}>
-        <img src={ require(img) } width='200px' height='110px' />
+      <Grid item xs={3}>
+        <img src={ require('../home.png') } width='190px' height='110px' />
       </Grid>
-      <Grid item xs={6}>
-        <Typography align='left'>{name}</Typography>
+      <Grid item xs={7}>
+      <Typography align='left' margin='1.5rem 0px 0px 1rem' fontWeight={'bold'}> {name} </Typography>
+        <Typography align='left' margin='0rem 0px 0px 1rem' fontSize={'12px'}> { num_places } </Typography>
       </Grid>
       <Grid item xs={2}>
-        <Item>Remove</Item>
+        <RemoveItem>Remove</RemoveItem>
       </Grid>
     </Grid>
   )
