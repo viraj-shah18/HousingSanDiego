84d6fa313942f3004555c389b1c0efc276ad6ed1
Viraj Shah <viraj0605@gmail.com>
2023-03-07 18:03:56 -0800

    made the list dynamic, click on add will add new collection

--- frontend/src/components/collection.js
+++ frontend/src/components/collection.js
@@ -5,8 +5,9 @@ import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Divider from '@mui/material/Divider';
-// import axios from 'axios';
+import axios from 'axios';
 import TextField from '@mui/material/TextField';
+import { useLocation } from "react-router-dom";
 
 
 const style = {
@@ -39,9 +40,7 @@ const RemoveItem = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
-function CollectionList(props) {
-  console.log(props);
-
+function CollectionListComponent(props) {
   return (
     <Grid container spacing={2}>
       <Grid item xs={3}>
@@ -61,17 +60,55 @@ function CollectionList(props) {
 
 export default function Collection () {
     // from backend -> Need list of ids for collections present
+    // await FetchCollections();
+    const [collectionsList, setcollectionList] = useState([])
     
-    const collection_names = ["Houses near UCSD", "Houses near Ralphs", "3 Bedrooms" , "4 Bedrooms", "Inexpensive options"];
-    const num_places = [1, 2, 3, 4, 5];
-    const images = ['../imgs/home.jpg', '../collectionpage.png', '../home.png', '../findroommate.png', '../imgs/home.jpg'];
+    // const collection_names = ["Houses near UCSD", "Houses near Ralphs", "3 Bedrooms" , "4 Bedrooms", "Inexpensive options"];
+    // const num_places = [1, 2, 3, 4, 5];
+    // const images = ['../imgs/home.jpg', '../collectionpage.png', '../home.png', '../findroommate.png', '../imgs/home.jpg'];
 
-    let collections_list = [];
-    for (var i = 0; i < collection_names.length; i++){
-      collections_list.push(<CollectionList name={collection_names[i]} num_places={num_places[i]} img={images[i]} />)
-    }
-    console.log(collections_list);
+    function AddCollectionInput(props) {
+      const [textInput, setTextInput] = useState('');
+      
+      const handleTextInputChange = event => {
+        setTextInput(event.target.value);
+      };
+      
+      const sendTextInput = event => {
+        // create api request here
+        setTextInput('');
+        setcollectionList([
+          ...collectionsList,
+          { name: textInput, num_places: 0, img: ''}
+        ]);
 
+        props.collections_list.push(textInput);
+        props.num_places.push(0);
+        props.img.push('../imgs/home.jpg');
+    
+      }
+      
+      return (
+        <Box paddingTop={'2rem'} >
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <Typography paddingTop={'0.4em'} fontSize={'16px'} fontWeight={'bold'}>
+                Create new collection: 
+              </Typography>
+            </Grid>
+            <Grid item xs={7}>
+              <TextField fullWidth size='small' id="outlined-basic" label="New Collection Name" variant="outlined" value= {textInput} onChange= {handleTextInputChange}/>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography>
+                <AddItem onClick={sendTextInput}>Add</AddItem>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      )
+    };
+    
     
     return (
       <Box sx={style}>
@@ -80,7 +117,7 @@ export default function Collection () {
         </Typography>
         <Divider variant="middle" sx={{ p:1}}/> 
         <Box sx={{ flexGrow: 1, maxHeight: 300, overflow: 'auto' }}>
-          {collections_list}
+          {collectionsList.map(collection => (<CollectionListComponent name={collection.name} num_places={collection.num_places} img={collection.img}/>))}
         </Box>
         <Divider variant="middle" sx={{ p:0.2}}/> 
         <AddCollectionInput/>
@@ -88,50 +125,16 @@ export default function Collection () {
 }   
 
 
-// export async function FetchCollections(userId) {
-  //   let data;
-//   await axios.get('http://localhost:8000/api/user_collection/${userId}')
-//     .then(res => {
-  //       console.log({res})
-  //       data = res.data;
-  //       console.log({data})
-//   })
-//   .catch(err => {})
-//   return (data);
-// }
-
-
-function AddCollectionInput() {
-  const [textInput, setTextInput] = useState('');
-  
-  const handleTextInputChange = event => {
-    setTextInput(event.target.value);
-    console.log({textInput});
-  };
-  
-  const sendTextInput = event => {
-    // create api request here
-    console.log("Button clicked", textInput);
-    setTextInput('');
-  }
-  
-  return (
-    <Box paddingTop={'2rem'} >
-      <Grid container spacing={2}>
-        <Grid item xs={3}>
-          <Typography paddingTop={'0.4em'} fontSize={'16px'} fontWeight={'bold'}>
-            Create new collection: 
-          </Typography>
-        </Grid>
-        <Grid item xs={7}>
-          <TextField fullWidth size='small' id="outlined-basic" label="New Collection Name" variant="outlined" value= {textInput} onChange= {handleTextInputChange}/>
-        </Grid>
-        <Grid item xs={2}>
-          <Typography>
-            <AddItem onClick={sendTextInput}>Add</AddItem>
-          </Typography>
-        </Grid>
-      </Grid>
-    </Box>
-  )
-};
+export async function FetchCollections() {
+  const location = useLocation();
+  const username = location.state.username;
+  let data;
+  await axios.get('http://localhost:8000/api/user_collection/', )
+    .then(res => {
+        console.log({res})
+        data = res.data;
+        console.log({data})
+  })
+  .catch(err => {})
+  return (data);
+}
