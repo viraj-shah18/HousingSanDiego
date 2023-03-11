6029cf8da50de25474e8c6c0710b93432f8726fe
Viraj Shah <viraj0605@gmail.com>
2023-03-10 22:35:34 -0800

    added link to collections page from popup

--- frontend/src/components/collection.js
+++ frontend/src/components/collection.js
@@ -7,7 +7,8 @@ import Paper from '@mui/material/Paper';
 import Divider from '@mui/material/Divider';
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
-import { useLocation } from "react-router-dom";
+import {useNavigate, useLocation } from "react-router-dom";
+
 
 
 const style = {
@@ -31,7 +32,7 @@ const RemoveItem = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
-  const AddItem = styled(Paper)(({ theme }) => ({
+const AddItem = styled(Paper)(({ theme }) => ({
     backgroundColor: '#24d42b',
     ...theme.typography.body2,
     padding: theme.spacing(1),
@@ -40,13 +41,28 @@ const RemoveItem = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
+const clickable = {
+  cursor: 'pointer'
+};
+
+
 function CollectionListComponent(props) {
+  const navigate = useNavigate();
+  
+  const handleCollectionsClick = event => {
+    navigate('/collections', {
+      state: {
+        collectionId: event.target.value
+      }
+    })
+  }
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={3}>
         <img src={ props.img } width='190px' height='110px' alt="No alt text"/>
       </Grid>
-      <Grid item xs={7}>
+      <Grid item xs={7} value={props.id} onClick={handleCollectionsClick} sx={clickable}>
       <Typography align='left' margin='1.5rem 0px 0px 1rem' fontWeight={'bold'}> {props.name} </Typography>
         <Typography align='left' margin='0rem 0px 0px 1rem' fontSize={'12px'}> { props.num_places } places</Typography>
       </Grid>
@@ -60,15 +76,18 @@ function CollectionListComponent(props) {
 
 export default function Collection () {
     // from backend -> Need list of ids for collections present
-    // await FetchCollections();
+    // let allcollections = FetchCollections();
+    let currentMax = 0;
     const [collectionsList, setcollectionList] = useState([])
+    // setcollectionList(...collectionsList, ...allcollections)
     
     // const collection_names = ["Houses near UCSD", "Houses near Ralphs", "3 Bedrooms" , "4 Bedrooms", "Inexpensive options"];
     // const num_places = [1, 2, 3, 4, 5];
     // const images = ['../imgs/home.jpg', '../collectionpage.png', '../home.png', '../findroommate.png', '../imgs/home.jpg'];
 
-    function AddCollectionInput(props) {
+    function AddCollectionInput() {
       const [textInput, setTextInput] = useState('');
+      const [currMax, setMax] = useState(0);
       
       const handleTextInputChange = event => {
         setTextInput(event.target.value);
@@ -76,16 +95,15 @@ export default function Collection () {
       
       const sendTextInput = event => {
         // create api request here
+        if (textInput === ''){
+          return;
+        }
+        setMax(currMax+1);
         setTextInput('');
         setcollectionList([
           ...collectionsList,
-          { name: textInput, num_places: 0, img: ''}
+          {id: currMax, name: textInput, num_places: 0, img: ''}
         ]);
-
-        props.collections_list.push(textInput);
-        props.num_places.push(0);
-        props.img.push('../imgs/home.jpg');
-    
       }
       
       return (
@@ -109,7 +127,7 @@ export default function Collection () {
       )
     };
     
-    
+    console.log({collectionsList  })
     return (
       <Box sx={style}>
         <Typography id='modal-modal-title' variant='h5' component='h2' align='center'>
@@ -117,7 +135,7 @@ export default function Collection () {
         </Typography>
         <Divider variant="middle" sx={{ p:1}}/> 
         <Box sx={{ flexGrow: 1, maxHeight: 300, overflow: 'auto' }}>
-          {collectionsList.map(collection => (<CollectionListComponent name={collection.name} num_places={collection.num_places} img={collection.img}/>))}
+          {collectionsList.map(collection => (<CollectionListComponent id={collection.id} name={collection.name} num_places={collection.num_places} img={collection.img}/>))}
         </Box>
         <Divider variant="middle" sx={{ p:0.2}}/> 
         <AddCollectionInput/>
@@ -127,9 +145,13 @@ export default function Collection () {
 
 export async function FetchCollections() {
   const location = useLocation();
-  const username = location.state.username;
+  console.log({location});
+  let auth= {
+    'username': location.state.loginInfo.username,
+    'password': location.state.loginInfo.password
+  }
   let data;
-  await axios.get('http://localhost:8000/api/user_collection/', )
+  await axios.get('http://localhost:8000/api/user_collection/', {auth:auth})
     .then(res => {
         console.log({res})
         data = res.data;
--- frontend/src/components/collectionList.js
+++ frontend/src/components/collectionList.js
@@ -1,8 +1,13 @@
 //This page is for collection lists
 //This page could be similar to search page but its results are based on the collections
 import * as React from 'react';
+import {useNavigate,useLocation} from 'react-router-dom';
+
 
 function CollectionPage() {
+    const location = useLocation();
+    console.log({location});
+    
     return (
         <div>
             <text>This is CollectionPage!!</text>
